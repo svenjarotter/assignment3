a3f335601e5b26d6eed1e89f279763ed8361c2e0
svenjarotter <39273939+svenjarotter@users.noreply.github.com>
2018-06-19 16:33:35 +0200

    Add files via upload

---
+++ ba2b.py
@@ -0,0 +1,61 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+>>> k = 6
+DNA = ["TGATGATAACGTGACGGGACTCAGCGGCGATGAAGGATGAGT",
+"CAGCGACAGACAATTTCAATAATATCCGCGGTAAGCGGCGTA",
+"TGCAGAGGTTGGTAACGCCGGCGACTCGGAGAGCTTTTCGCT",
+"TTTGTCATGAACTCAGATACCATAGAGCACCGGCGAGACTCA",
+"ACTGGGACTTCACATTAGGTTGAACCGCGAGCCAGGTGGGTG",
+"TTGCGGACGGGATACTCAATAACTAAGGTAGTTCAGCTGCGA",
+"TGGGAGGACACACATTTTCTTACCTCTTCCCAGCGAGATGGC",
+"GAAAAAACCTATAAAGTCCACTCTTTGCGGCGGCGAGCCATA",
+"CCACGTCCGTTACTCCGTCGCCGTCAGCGATAATGGGATGAG",
+"CCAAAGCTGCGAAATAACCATACTCTGCTCAGGAGCCCGATG"]
+
+def hammingDistance(a, b):
+	ham = 0
+	for x, y in zip(a, b):
+		if x != y:
+			ham += 1
+	return ham
+
+def distanceBetweenPatternAndString(pattern, DNA):
+	k = len(pattern)
+	distance = 0
+	for x in DNA:
+		hamming = k+1
+		for i in range(len(x) - k + 1):
+			z = hammingDistance(pattern, x[i:i+k])
+			if hamming > z:
+				hamming = z
+		distance += hamming
+	return distance
+
+def numberToSymbol(x):
+	if x == 0:
+		return "A"
+	if x == 1:
+		return "C"
+	if x == 2:
+		return "G"
+	if x == 3:
+		return "T"
+
+def numberToPattern(x, k):
+	if k == 1:
+		return numberToSymbol(x)
+	return numberToPattern(x // 4, k-1) + numberToSymbol(x % 4)
+    
+def medianString(DNA, k):
+	distance = (k+1) * len(DNA)
+	median = ""
+	for i in range(4**k):
+		pattern = numberToPattern(i, k)
+		z = distanceBetweenPatternAndString(pattern, DNA)
+		if distance > z:
+			distance = z
+			median = pattern
+	return median
+
+print(medianString(DNA, k))
